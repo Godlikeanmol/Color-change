dbb67cc272caad62b86adaea817c84366f2086d4
Anmol Jat <126661635+Godlikeanmol@users.noreply.github.com>
2023-09-08 23:19:21 +0530

    add

---
+++ script.js
@@ -0,0 +1,36 @@
+var rect = document.querySelector('.color');
+
+        rect.addEventListener("mousemove",function(et){
+            var rectlocation = rect.getBoundingClientRect();
+            var left = et.x - rectlocation.left;
+            var top = et.y -rectlocation.top;
+            console.log(right)
+            document.querySelector("#box").innerHTML=left;
+            document.querySelector("#box2").innerHTML=top;
+
+            if(left < rectlocation.width/2){
+                document.querySelector("#left").innerHTML="left"
+                rect.style.backgroundColor=`rgb(${left},${top},${left})`;
+                rect.style.transition
+            }
+            else{
+                document.querySelector("#left").innerHTML="right";
+                rect.style.backgroundColor=`rgb(${left}, ${top} , 20)`
+            }
+            chnagepagetittle();
+            function chnagepagetittle(){
+            PageTittle = "colourfull";
+                document.title=PageTittle;
+            }
+        });
+
+        
+        rect.addEventListener("mouseleave", function(){
+            rect.style.backgroundColor="#fff";
+
+            chnagepagetittle();
+            function chnagepagetittle(){
+            PageTittle = "White";
+                document.title=PageTittle;
+            }
+        })
